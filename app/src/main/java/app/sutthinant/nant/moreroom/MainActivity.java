b08da0c226fb677d35bdebf1d024fba523f32e2a
nant <sutthinant@gmail.com>
2017-07-16 11:41:56 +0700

    show Image OK

--- app/src/main/java/app/sutthinant/nant/moreroom/MainActivity.java
+++ app/src/main/java/app/sutthinant/nant/moreroom/MainActivity.java
@@ -1,7 +1,12 @@
 package app.sutthinant.nant.moreroom;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -11,7 +16,11 @@ public class MainActivity extends AppCompatActivity {
     //Explicit
     private String nameString, priceString, phoneString;
     private EditText nameEditText, priceEditText, phoneEditText;
+    private int indexAnInt = 0;
+    private int[] picInts = new int[]{R.id.imvShowPic1,
+            R.id.imvShowPic2, R.id.imvShowPic3, R.id.imvShowPic4,};
 
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +29,69 @@ public class MainActivity extends AppCompatActivity {
         //Back Controller
         backController();
 
-        //Initail View
+        //Initial View
 
         initailView();
 
+        //Add Picture Controller
+        addPictureController();
+
 
     }   //Main Method
+
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        String tag = "16JulyV1";
+        Log.d(tag, "requestCode ==>" + requestCode);
+
+
+
+        if (requestCode == indexAnInt && resultCode == RESULT_OK) {
+            Log.d(tag, "All Result OK");
+
+
+            //Show Image
+            uri = data.getData();
+            try {
+
+                Bitmap bitmap = BitmapFactory
+                        .decodeStream(getContentResolver().openInputStream(uri));
+                ImageView imageView = (ImageView) findViewById(picInts[indexAnInt]);
+                bitmap = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+                imageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                Log.d(tag, "e ShowImage ==>" + e.toString());
+            }
+            indexAnInt += 1;
+        }   //if
+
+
+    }  //OnActivityResult
+
+    private void addPictureController() {
+        ImageView imageView = (ImageView) findViewById(R.id.imvadd);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (indexAnInt < picInts.length) {
+                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    intent.setType("image/*");
+                    startActivityForResult(Intent.createChooser(intent, "Please Choose App"),
+                            indexAnInt);
+                } else {
+                    MyAlert myAlert = new MyAlert(MainActivity.this);
+                    myAlert.myDialog("Over Picture", "Can't Add More"+ Integer.toString(picInts.length)+"Pic");
+                }
+
+            }
+        });
+    }
 
     private void initailView() {
         nameEditText = (EditText) findViewById(R.id.editName);
