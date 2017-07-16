9efbdc469f8b70d7a0f4e9e11627d33a32b68c64
nant <sutthinant@gmail.com>
2017-07-16 10:40:28 +0700

    check space OK

---
+++ app/src/main/java/app/sutthinant/nant/moreroom/MyAlert.java
@@ -0,0 +1,38 @@
+package app.sutthinant.nant.moreroom;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by NantHome on 7/16/2017.
+ */
+
+public class MyAlert {
+
+    private Context context;
+
+    public MyAlert(Context context) {
+        this.context = context;
+    }
+
+    public void myDialog(String strTitle, String strMessage) {
+
+        MyConstant myConstant = new MyConstant();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(myConstant.getIconAlert());
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        builder.show();
+
+    }
+
+} //Main Class
