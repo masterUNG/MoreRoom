eecdba1f35243cb64858d199647459ad89ae825c
MasterUNG <phrombutr@gmail.com>
2017-08-26 14:24:52 +0700

    Get JSON OK

--- app/src/main/java/app/sutthinant/nant/moreroom/ListRoomFragment.java
+++ app/src/main/java/app/sutthinant/nant/moreroom/ListRoomFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * Created by masterung on 8/26/2017 AD.
@@ -28,8 +30,32 @@ public class ListRoomFragment extends Fragment{
         //Create ToolBar
         createToolBar();
 
+        //Create ListView
+        createListView();
 
-    }
+
+    }   // onActivityCreated
+
+    private void createListView() {
+        ListView listView = getView().findViewById(R.id.livRoom);
+        MyConstant myConstant = new MyConstant();
+        String strURL = myConstant.getUrlGetRoomString();
+        String tag = "26AugV1";
+
+        try {
+
+            GetAllData getAllData = new GetAllData(getActivity());
+            getAllData.execute(strURL);
+            String strJSON = getAllData.get();
+            Log.d(tag, "JSoN ==> " + strJSON);
+
+        } catch (Exception e) {
+            Log.d(tag, "e createListview ==> " + e.toString());
+        }
+
+
+
+    }   // createListView
 
     private void createToolBar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarListRoom);
---
+++ app/src/main/java/app/sutthinant/nant/moreroom/RoomListViewAdapter.java
@@ -0,0 +1,65 @@
+package app.sutthinant.nant.moreroom;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by masterung on 8/26/2017 AD.
+ */
+
+public class RoomListViewAdapter extends BaseAdapter{
+
+    private Context context;
+    private String[] iconStrings, nameStrings, priceStrings;
+
+    public RoomListViewAdapter(Context context,
+                               String[] iconStrings,
+                               String[] nameStrings,
+                               String[] priceStrings) {
+        this.context = context;
+        this.iconStrings = iconStrings;
+        this.nameStrings = nameStrings;
+        this.priceStrings = priceStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return nameStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = layoutInflater.inflate(R.layout.room_listview_layout, viewGroup, false);
+
+        //For Image
+        ImageView imageView = view1.findViewById(R.id.imvIcon);
+        Picasso.with(context).load(iconStrings[i]).into(imageView);
+
+        //For Text
+        TextView nameTextView = view1.findViewById(R.id.txtName);
+        TextView priceTextView = view1.findViewById(R.id.txtPrice);
+        nameTextView.setText(nameStrings[1]);
+        priceTextView.setText(priceStrings[i]);
+
+        return view1;
+    }
+}   // Main Class
