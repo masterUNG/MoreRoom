c4352cbd4ef813ee34954183df0b3233459706bc
MasterUNG <phrombutr@gmail.com>
2017-08-26 13:44:30 +0700

    Back Contorller OK

--- app/src/main/java/app/sutthinant/nant/moreroom/ListRoomFragment.java
+++ app/src/main/java/app/sutthinant/nant/moreroom/ListRoomFragment.java
@@ -3,6 +3,7 @@ package app.sutthinant.nant.moreroom;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,5 +19,31 @@ public class ListRoomFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_listroom, container, false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Create ToolBar
+        createToolBar();
+
+
+    }
+
+    private void createToolBar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarListRoom);
+        ((ListRoomActivity) getActivity()).setSupportActionBar(toolbar);
+        ((ListRoomActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((ListRoomActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.mipmap.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
+
+
     }
 }   // Main Class
